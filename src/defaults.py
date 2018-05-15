cd6c5c039e4b1a75cc395b1fe4d2d31c3aa517f6
Unknown <gelotus@gmail.com>
2018-05-15 15:23:26 +0200

    Rename to CoreElec

--- src/defaults.py
+++ src/defaults.py
@@ -1,29 +1,22 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
+#      This file is part of CoreELEC - http://coreelec.org
+#      Copyright (C) 2018-present CoreELEC (mail (at) coreelec.org)
 #      Copyright (C) 2009-2013 Stephan Raue (stephan@openelec.tv)
 #      Copyright (C) 2013 Lutz Fiebach (lufie@openelec.tv)
 #
-#  This program is dual-licensed; you can redistribute it and/or modify
+#  CoreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  CoreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC; see the file COPYING.  If not, see
-#  <http://www.gnu.org/licenses/>.
-#
-#  Alternatively, you can license this library under a commercial license,
-#  please contact OpenELEC Licensing for more information.
-#
-#  For more information contact:
-#  OpenELEC Licensing  <license@openelec.tv>  http://www.openelec.tv
+#  along with CoreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-# -*- coding: utf-8 -*-
 
 import os
 
@@ -41,7 +34,7 @@ USER_CONFIG = os.environ.get('USER_CONFIG', '/storage/.config')
 
 connman = {
     'CONNMAN_DAEMON': '/usr/sbin/connmand',
-    'WAIT_CONF_FILE': '%s/libreelec/network_wait' % CONFIG_CACHE,
+    'WAIT_CONF_FILE': '%s/coreelec/network_wait' % CONFIG_CACHE,
     'ENABLED': lambda : (True if os.path.exists(connman['CONNMAN_DAEMON']) else False),
     }
 
@@ -88,7 +81,7 @@ system = {
     'LOCAL_UPDATE_DIR': '/storage/.update/',
     'GET_CPU_FLAG': "cat /proc/cpuinfo | grep -q 'flags.* lm ' && echo '1' || echo '0'",
     'XBMC_RESET_FILE': '%s/reset_xbmc' % CONFIG_CACHE,
-    'LIBREELEC_RESET_FILE': '%s/reset_oe' % CONFIG_CACHE,
+    'COREELEC_RESET_FILE': '%s/reset_oe' % CONFIG_CACHE,
     'KEYBOARD_INFO': '/usr/share/X11/xkb/rules/base.xml',
     'UDEV_KEYBOARD_INFO': '%s/xkb/layout' % CONFIG_CACHE,
     'NOX_KEYBOARD_INFO': '/usr/lib/keymaps',
